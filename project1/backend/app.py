6712540e448aa214e1e897954b3b2866974944ff
momoein <momoein711@gmail.com>
2025-06-12 14:34:50 +0330

    fix: somting

--- project1/backend/app.py
+++ project1/backend/app.py
@@ -1,17 +1,13 @@
 from flask import Flask, jsonify, request, abort
-from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from util import load_queries
 import os
 
 app = Flask(__name__)
-CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'storage.db')}"
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/momoein/desk/projects/DataBase4032/project1/backend/storage.db'
 db = SQLAlchemy(app)
 
 class Student(db.Model):
