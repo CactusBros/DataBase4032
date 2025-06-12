ec59e017c67594738bcfd2122c4618db4d3d130f
Amir Shakibafar <149489707+AmirShakibafar@users.noreply.github.com>
2025-06-12 10:37:20 +0330

    finished overall frontend structure

--- project1/frontend/src/App.jsx
+++ project1/frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import Navbar from "./components/Navbar";
+// src/App.js
+import { useState } from "react";
 import Hero from "./components/Hero";
 import Options from "./components/Options";
+import ResultsTable from "./components/ResultsTable";
 
 function App() {
+  const [tableHeaders, setTableHeaders] = useState([]);
+  const [tableData, setTableData] = useState([]);
+
+  const handleQueryResults = (headers, data) => {
+    setTableHeaders(headers);
+    setTableData(data);
+  };
+
   return (
     <main>
       <Hero />
-      <Options/>
+      <Options onQuery={handleQueryResults} />
+      <ResultsTable headers={tableHeaders} data={tableData} />
     </main>
   );
 }
